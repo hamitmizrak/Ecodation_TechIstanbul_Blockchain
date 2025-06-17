d6221e0b012d8071da6ac6af6dd2ebcc2421777f
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-17 21:08:43 +0300

    markdown

--- src/main/java/com/hamitmizrak/dto/Deneme.java
+++ src/main/java/com/hamitmizrak/dto/Deneme.java
@@ -1,4 +1,20 @@
 package com.hamitmizrak.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Deneme {
+
+    private String name;
+
+    public static void main(String[] args) {
+        Deneme deneme= new Deneme();
+        deneme.setName("Hamit Mızrak");
+        System.out.println(deneme.getName());
+        System.out.println("Merhabalar");
+    }
 }
---
+++ tutorials_blockchain/_01_blockchain.md
@@ -0,0 +1,5 @@
+# Blockchain
+
+[Github](http://)
+
+---

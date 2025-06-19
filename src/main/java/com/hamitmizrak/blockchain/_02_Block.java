44a8d2f1e9c78aa965bdc96e039312fa2f264a58
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-19 19:58:39 +0300

    1- Transaction 2- Block

--- src/main/java/com/hamitmizrak/blockchain/_02_Block.java
+++ src/main/java/com/hamitmizrak/blockchain/_02_Block.java
@@ -1,5 +1,8 @@
 package com.hamitmizrak.blockchain;
 
+import org.controlsfx.tools.Utils;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -20,22 +23,52 @@ public class _02_Block {
     // Zincirdeki sıra sayısını tutar. Genesis: Block 0 olur, bundan sonraki yapılarda 1,2,3..
     private int index;
 
-    // Block'un oluşturulma zamanı
+    // Block'un oluşturulma zamanı (tarih -saat)
     private String timestamp;
 
     // Blok içindeki yer alan işlemlerin (transfer, veri vs) listelendi
     private List<_01_Transaction> transactions;
 
-    // ;Önceki bloğun SHA-256 hash değerini tutacaktır.
+    // Bu bloktan önce gelen bloğun hash değeri
+    // Önceki bloğun SHA-256 hash değerini tutacaktır.
     private String previousHash;
 
-    // Bu bloğun kendi hash değeri olarak karşımıza geliyor.
+    // Bu bloğun kendi hash değeri olarak karşımıza geliyor(Blog kimliği)
     private String hash;
 
     // CONSTRUCTOR
 
+    public _02_Block(int index, List<_01_Transaction> transactions, String previousHash) {
+        this.index = index;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
+        this.timestamp= LocalDateTime.now().toString();
+        this.hash = calculateHash(); // Hash hesaplansın ve atansın
+    }
+
+
     // METHOD
+    // calculateHash
+    private String calculateHash() {
+        //
+        StringBuilder txData= new StringBuilder();
+        for(_01_Transaction temp : transactions){
+            txData.append(temp.toString()); // Her transaction ekle
+        }
+
+        // Hash girdisi : index + zaman + işlem verisi+ önceki hash
+        String input = index +timestamp +txData + previousHash;
+        return Utils   // Yardımcı sınıf ile hash hesaplanan
+    }
+
+    // Blok hash içeriğinde verilerin tutarlı olup olmadığını doğrulasın.
+    public boolean isValid(){
+        return hash.equals(calculateHash());
+    }
 
     // GETTER/SETTER
-
-}
+    // Bloktaki işlemleri dönder
+    public List<_01_Transaction> getTransactions() {
+        return transactions;
+    }
+} // end _02_Block

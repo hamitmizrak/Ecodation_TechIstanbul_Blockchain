b91205ff3cd0a649cbca662e300cd3737cff40e5
Hamit Mizrak <hamitmizrak@gmail.com>
2025-06-24 21:18:58 +0300

    1- Transaction 2- Utils 3- Block 4- Blockchain 5- Wallet 6- Smart Contract 7- Main Test-1 8- Main Test-2

--- src/main/java/com/hamitmizrak/blockchain/_07_2_MainTest.java
+++ src/main/java/com/hamitmizrak/blockchain/_07_2_MainTest.java
@@ -11,22 +11,25 @@ public class _07_2_MainTest {
     // Running
     private static boolean running =true;
 
+    // UserData
+    private static String[] userData= new String[3];
+
     // Blockchain ve smart Contract
     static _04_Blockchain blockchain = new _04_Blockchain();
     static _06_SmartContract smartContract= new _06_SmartContract(blockchain);
 
     // Kullanıcı ekle
     private static String[] addUser() {
-        String[] userData= new String[3];
         for (int i = 0; i < 3; i++) {
             System.out.print(SpecialColor.BLUE+"Lütfen " + (i+1)+ " kullanıcıyı yazınız: "+SpecialColor.RESET);
             String temp = scanner.nextLine();
             userData[i]=temp;
         }
+        // Kullanıcıları Göster
         for (String temp: userData){
             System.out.print(temp+" ");
         }
-        scanner.close();
+        //scanner.close();
         return userData;
     }
 
@@ -40,6 +43,7 @@ public class _07_2_MainTest {
 
     // Chooise
     private static void userChooise(){
+        walletAdd();
         while (running){
             System.out.println("\n ========== Blockchain Menüsü =========");
             System.out.println("1- Transfer Yap");
@@ -55,22 +59,50 @@ public class _07_2_MainTest {
 
             switch (chooise){
                 case 1:
-                    System.out.print("Gönderen");
+                    System.out.print("Gönderen: ");
                     String from = scanner.nextLine();
-                    System.out.print("Alıcı ");
+                    System.out.print("Alıcı: ");
                     String to = scanner.nextLine();
                     System.out.print("Miktar: ");
                     double amount = scanner.nextDouble();
                     scanner.nextLine(); //dummy enter
                     System.out.println(smartContract.executeTransfer(from,to,amount));
                     break;
+
+                case 2:
+                    System.out.println("\n=========Blockchain Yapısı========");
+                    System.out.println(blockchain.printBlockChain());
+                    break;
+
+                case 3:
+                    System.out.println("\n=========Cüzdan Bakiyesi========");
+                    _05_Wallet.getBalances().forEach((user, balance)->{
+                        System.out.println(user+ " ==> Bakiyesi: "+blockchain);
+                    });
+                    break;
+
+                case 4:
+                    String result = blockchain.isChainValid() ? "✅ Evet": "❌ Hayır";
+                    System.out.println("\nZincir Geçerli mi ? "+(result));
+                    break;
+
+                case 0:
+                    running=false;
+                    System.out.println("Çıkış yapılıyor");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("❌ Geçersiz seçim yaptınız tekrar deneyiniz ");
             }
         }
+
+        scanner.close();
     }
 
     // psvm
     public static void main(String[] args) {
-        walletAdd();
+        userChooise();
 
     }
 }
